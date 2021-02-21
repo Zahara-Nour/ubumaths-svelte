e2b58a1c01088c2568e88fc05222ca531b4d9ce5
David Le Jolly <zahara.alnour@gmail.com>
2021-02-21 06:17:54 +0300

    calcul mental

--- src/features/calcul-mental/questions.js
+++ src/features/calcul-mental/questions.js
@@ -108,6 +108,113 @@ export default {
           defaultDelay: 10
         },
       ]
-    }
+    },
+    Soustraction:{
+        "Résultat":[
+              {
+                
+                description: "Trouver le résultat d'une soustraction (résultat positif)",
+                subdescription: "Nombres à 1 chiffre",
+                expressions: ["&1-&2"],
+                variables: { "&1": "$e[2;9]", "&2":"$e[1;&1-1]"},
+                "defaultDelay": 10,
+              },
+              {
+                "description": "Trouver le résultat d'une soustraction (résultat positif)",
+                "description": "Nombres à 2 chiffres sans retenue à effectuer",
+                "expressions": ["${ $e[2;9]*10 + $e[2;9] } - ${ $e[1;$1-1]*10 + $e[1;$2-1] }"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 15,
+                "point": 1
+              },
+              {
+                "level": 3,
+                "description": "Nombres à 3 chiffres sans retenue",
+                "enounce": "",
+                "expressions": ["${ $e[2;9]*100 + $e[2;9]*10 + $e[2;9] } - ${ $e[1;$1-1]*100 + $e[1;$2-1]*10 + $e[1;$3-1] }"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 20,
+                "point": 1
+              },
+              {
+                "level": 4,
+                "description": "Nombres à 2 chiffres",
+                "enounce": "",
+                "expressions": ["$e[11;99]-$e[10;$1-1]"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 15,
+                "point": 1
+              },
+              {
+                "level": 5,
+                "description": "Nombres à 3 chiffres",
+                "enounce": "",
+                "expressions": ["$e[101;999]-$e[100;$1-1]"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 20,
+                "point": 1
+              }
+            ]
+          },
+          {
+            "label": "A trou",
+            "levels": [
+              {
+                "level": 1,
+                "description": "Nombres à 1 chiffre.",
+                "enounce": "",
+                "expressions": ["?-$e[2;8]=$e[1;9-$1]","$e[2;9]-?=$e[1;$1-1]" ],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 10,
+                "point": 1
+              },
+              {
+                "level": 2,
+                "description": "Nombres à 2 chiffres sans retenue.",
+                "enounce": "",
+                "expressions": ["${ $e[2;9]*10 + $e[2;9] } - ? =  ${ $e[1;$1-1]*10 + $e[1;$2-1] }"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 15,
+                "point": 1
+              },
+              {
+                "level": 3,
+                "description": "Nombres à 3 chiffres sans retenue.",
+                "enounce": "",
+                "expressions": ["${ $e[2;9]*100 + $e[2;9]*10 + $e[2;9] } - ? = ${ $e[1;$1-1]*100 + $e[1;$2-1]*10 + $e[1;$3-1] }"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 20,
+                "point": 1
+              },
+              {
+                "level": 4,
+                "description": "Nombres à 2 chiffres",
+                "enounce": "",
+                "expressions": ["?-$e[12;98]=$e[1;99-$1]","$e[12;99]-?=$e[1;$1-10]"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 15,
+                "point": 1
+              },
+              {
+                "level": 5,
+                "description": "Nombres à 3 chiffres",
+                "enounce": "",
+                "expressions": ["?-$e[102;998]=$e[1;999-$1]","$e[102;999]-?=$e[1;$1-100]"],
+                "correction": "value",
+                "answer": "decimal",
+                "defaultDelay": 20,
+                "point": 1
+              }
+            ]
+          }
+        ]
   }
 }
