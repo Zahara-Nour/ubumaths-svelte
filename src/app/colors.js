a3a1a2fed3df26f07973c1e2a8708b0ea976419c
David Le Jolly <zahara.alnour@gmail.com>
2021-10-30 14:43:22 +0300

    Add colors

--- src/app/colors.js
+++ src/app/colors.js
@@ -1,2 +1,10 @@
 export const primary='#6200ee'
 export const secondary = '#018786'
+
+export const beginner_class = "red lighten-1 white-text "
+export const apprentice_class = "amber lighten-1 white-text "
+export const confirmed_class = "green lighten-1 white-text "
+
+export const good_class = "green lighten-1 white-text "
+export const not_complete_class = "amber lighten-1 white-text "
+export const not_good_class = "red lighten-1 white-text "
