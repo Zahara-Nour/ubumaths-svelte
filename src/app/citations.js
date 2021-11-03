f1d96cb3d810a855f742553348797be270fd5f91
David Le Jolly <zahara.alnour@gmail.com>
2021-11-03 18:41:37 +0300

    Add citations

---
+++ src/app/citations.js
@@ -0,0 +1,35 @@
+export const citations = [
+    {
+        citation:"Le jeu est la forme la plus élevée de la recherche.",
+        author:'Albert Einstein'
+    },
+    {
+        citation:"Joue et tu deviendras sérieux.",
+        author:'Aristote'
+    },
+    {
+        citation:"Le jeu est la premier poème de l’existence.",
+        author:'Jean-Paul Sartre'
+    },
+    {
+        citation:"Les enfants n’ont point d’affaires plus sérieuses que leurs jeux.",
+        author:'Michel de Montaigne'
+    },
+    {
+        citation:"Une civilisation est inachevée si elle n’ajoute pas à l’art de bien travailler celui de bien jouer.",
+        author:'Georges Santayana'
+    },
+    {
+        citation:"l n’y a rien de plus sérieux qu’un élève qui apprend en jouant.",
+        author:'David Le Jolly'
+    },
+    {
+        citation:"Joue et tu deviendras sérieux.",
+        author:'Aristote'
+    },
+    {
+        citation:"Joue et tu deviendras sérieux.",
+        author:'Aristote'
+    },
+
+]
